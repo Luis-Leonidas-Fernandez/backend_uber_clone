3d2979d691216750d0c7012d302d6ac286fa7e59
Luis-Leonidas-Fernandez <fernandezluis303@gmail.com>
2023-05-01 10:14:46 -0300

    update connect mongoose

--- database/config.js
+++ database/config.js
@@ -4,6 +4,7 @@ const  mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
+        mongoose.set('strictQuery', true);
         await mongoose.connect( process.env.DB_CNN,             
         );
 
