cbd5599f4752cbf7ecb6e46b64cfa9bb5c9191a2
luis <fernandezluis303@gmail.com>
2023-06-19 09:42:36 -0300

    test 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,10 +79,10 @@ const login = async(req, res = response) => {
             ok: true,
             usuario: usuarioDB, 
             token,
-            urlMapbox:   urlMapboxKey,
-            tokenMapBox: tokenMapBoxKey,
-            idMapBox:    idMapBoxKey,
-            mapToken:    mapTokenKey
+            urlMapbox:   "https://api.mapbox.com/styles/v1/luisleonidas/clawgaqa5000f17n1ck3nlma9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNrbnhhcWFiazBpNGoycG40YzVmbng1dW8ifQ.H3n7NYagN5ZjNWTPZxJVAw",
+            tokenMapBox: "pk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNrbnhhcWFiazBpNGoycG40YzVmbng1dW8ifQ.H3n7NYagN5ZjNWTPZxJVAw",
+            idMapBox:    "sk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNsZHozNmNpdTBpMmgzbnBvbXpwazZqejQifQ.BmLWsc31KgWMRxGg6HQH3w",
+            mapToken:    "mapbox.mapbox-streets-v8"
         });
 
 
