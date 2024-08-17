85a7b95931ae80206d41dde6935156be40161f1c
Luis Leónidas Fernández <121168647+Luis-Leonidas-Fernandez@users.noreply.github.com>
2024-08-17 17:35:25 -0300

    Update token.js

--- tokens/token.js
+++ tokens/token.js
@@ -1,9 +1,9 @@
 const { response } = require('express');
 
-const urlMapboxKey="https://api.mapbox.com/styles/v1/luisleonidas/clawgaqa5000f17n1ck3nlma9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNrbnhhcWFiazBpNGoycG40YzVmbng1dW8ifQ.H3n7NYagN5ZjNWTPZxJVAw";
-const tokenMapBoxKey="pk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNrbnhhcWFiazBpNGoycG40YzVmbng1dW8ifQ.H3n7NYagN5ZjNWTPZxJVAw";
-const mapTokenKey="sk.eyJ1IjoibHVpc2xlb25pZGFzIiwiYSI6ImNsZHozNmNpdTBpMmgzbnBvbXpwazZqejQifQ.BmLWsc31KgWMRxGg6HQH3w";
-const idMapBoxKey="mapbox.mapbox-streets-v8";
+const urlMapboxKey="";
+const tokenMapBoxKey="";
+const mapTokenKey="";
+const idMapBoxKey="";
 
 
 
